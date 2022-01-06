c6b4c4aada559061bcd3c0946d7658c3c7c5ab79
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-07 00:41:44 +0200

    fixed task 2
    fixed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,24 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const listEl1 = document.createElement("li")
-listEl1.classList.add("item")
-listEl1.textContent = "Potatoes"
-const listEl2 = document.createElement("li")
-listEl2.classList.add("item")
-listEl2.textContent = "Mushroom"
-const listEl3 = document.createElement("li")
-listEl3.classList.add("item")
-listEl3.textContent = "Garlic"
-const listEl4 = document.createElement("li")
-listEl4.classList.add("item")
-listEl4.textContent = "Tomatos"
-const listEl5 = document.createElement("li")
-listEl5.classList.add("item")
-listEl5.textContent = "Herbs"
-const listEl6 = document.createElement("li")
-listEl6.classList.add("item")
-listEl6.textContent = "Condiments"
-
 const ingredientsList = document.querySelector("#ingredients");
-ingredientsList.append(listEl1, listEl2, listEl3, listEl4, listEl5, listEl6)
+console.log(ingredientsList)
+
+
+const listEl = ingredients.map(el => {
+  const cteatedLi = document.createElement("li");
+  cteatedLi.textContent = el;
+  cteatedLi.classList.add("item");
+  return cteatedLi;
+}
+
+)
+
+ingredientsList.append(...listEl);
+  
+  
+
+
